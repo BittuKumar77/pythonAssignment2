b490b5f24a06dc50f6a747a5a648a6b7fbce4d48
BittuKumar77 <110903496+BittuKumar77@users.noreply.github.com>
2022-08-23 18:31:51 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,7 @@
+#2. Write a python script to add multi line comments and print values of four variables, each in a new line. Variable contains any values.
+
+print("Multiline comment")
+
+"""
+This is a multiline comment
+"""
---
+++ 5.py
@@ -0,0 +1,10 @@
+"""5. Create four variables in a Python script and assign values of different data types to
+them. Write a Python script to print value, its type and id of each variable"""
+
+from calendar import c
+
+
+a="Bittu"
+b=20
+
+print(a,b)
